a9eb1ce524aa990cbb4b52c87e2a59d2cc91504c
preetham uppu <preethamuppu@preethams-air.attlocal.net>
2015-06-21 21:25:02 -0400

    Initial Commit

---
+++ DribbleClientTests/DribbleClientTests.swift
@@ -0,0 +1,36 @@
+//
+//  DribbleClientTests.swift
+//  DribbleClientTests
+//
+//  Created by preetham uppu on 6/21/15.
+//  Copyright (c) 2015 preetham uppu. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DribbleClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
